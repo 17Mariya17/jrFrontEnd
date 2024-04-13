c015e05708f085c8730fcf7d4450aba75ecb876b
17Mariya17 <146371186+17Mariya17@users.noreply.github.com>
2024-04-13 15:02:53 +0000

    exercise2_modals

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 // Excercise 1
+
 // Wrong answer alert
 function exercise1Check() {
   document.getElementById("alert_placeholder").innerHTML = `<div
@@ -21,4 +22,92 @@ function exercise1CheckCorrect() {
             Great job!
         </p>
         </div>`;
+}
+
+// Excercise 2
+
+// Function for modals
+function exercise2Check() {
+  // Correct answer
+  if (document.getElementById("correctAnswer").checked) {
+    document.getElementById(
+      "exampleModal"
+    ).innerHTML = `<div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <p class="modal-title fw-bold fs-5" id="exampleModalLabel">
+        Correct. Great job!
+        </p>
+        <button
+          type="button"
+          class="btn-close"
+          data-bs-dismiss="modal"
+          aria-label="Close"
+        ></button>
+      </div>
+    </div>
+  </div>`;
+  } else if (document.getElementById("wrongAnswer").checked) {
+    // Wrong answer
+    document.getElementById(
+      "exampleModal"
+    ).innerHTML = `<div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <p class="modal-title fw-bold fs-5" id="exampleModalLabel">
+          Please read carefully and try again
+        </p>
+        <button
+          type="button"
+          class="btn-close"
+          data-bs-dismiss="modal"
+          aria-label="Close"
+        ></button>
+      </div>
+      <div class="modal-body">
+        Review the material by clicking on the Review button
+      </div>
+      <div class="modal-footer">
+        <button
+          type="button"
+          class="btn btn-primary"
+          data-bs-dismiss="modal"
+        >
+          Review
+        </button>
+      </div>
+    </div>
+  </div>`;
+  } else {
+    // No answer
+    document.getElementById(
+      "exampleModal"
+    ).innerHTML = `<div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <p class="modal-title fw-bold fs-5" id="exampleModalLabel">
+          Please select an answer
+        </p>
+        <button
+          type="button"
+          class="btn-close"
+          data-bs-dismiss="modal"
+          aria-label="Close"
+        ></button>
+      </div>
+      <div class="modal-body">
+        Review the material by clicking on the Review button
+      </div>
+      <div class="modal-footer">
+        <button
+          type="button"
+          class="btn btn-primary"
+          data-bs-dismiss="modal"
+        >
+          Review
+        </button>
+      </div>
+    </div>
+  </div>`;
+  }
 }
