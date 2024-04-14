469ae0bdb75e29bc47fc726af877ab7bc863ea55
17Mariya17 <146371186+17Mariya17@users.noreply.github.com>
2024-04-14 19:53:35 +0000

    exercise3_flex_boxes

---
+++ js/sortable3.js
@@ -0,0 +1,16 @@
+// Sortable lists - you can pass element between them
+//  Intoxicated
+Sortable.create(intoxicatedList, {
+  group: "exercise3Lists",
+  animation: 100,
+});
+// Not Intoxicated
+Sortable.create(notIntoxicatedList, {
+  group: "exercise3Lists",
+  animation: 100,
+});
+// Start list
+Sortable.create(startList, {
+  group: "exercise3Lists",
+  animation: 100,
+});
